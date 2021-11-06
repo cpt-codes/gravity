971a6309991d877299917901072a2ae23260239c
cpt-codes <62712045+cpt-codes@users.noreply.github.com>
2021-11-06 14:05:35 +0000

    Added Gravity class
    - The Gravity class contains functions that compute gravitational acceleration for different models.
    - Added namespace alias of boost::numeric::ublas to Vector.h to access ublas operations easily.
    - Cleaned up Vector.h dependencies.

--- include/gravity/Vector.h
+++ include/gravity/Vector.h
@@ -2,9 +2,7 @@
 #define GRAVITY_INCLUDE_GRAVITY_VECTOR_H_
 
 #include <cstddef>
-#include <stdexcept>
 
-#include <boost/format.hpp>
 #include <boost/numeric/ublas/vector.hpp>
 
 namespace gravity
@@ -12,8 +10,11 @@ namespace gravity
     // Dimensions of spatial vectors in the simulation
     constexpr std::size_t Dimensions{ 3 };
 
+    // Alias for boost::numeric::ublas
+    namespace ublas = boost::numeric::ublas;
+
     // Boost uBLAS vector of doubles
-    using Vector = boost::numeric::ublas::vector<double>;
+    using Vector = ublas::vector<double>;
 }
 
 #endif //GRAVITY_INCLUDE_GRAVITY_VECTOR_H_
